ea768f34b9be222c9a0a14bee1883bc48c85581b
mdcl <manuel.lindo@gmv.com>
2021-09-06 16:32:47 +0100

    change script name

---
+++ ship_tracking/server/reader_script.py
@@ -0,0 +1,34 @@
+import os
+import csv
+import json
+import requests
+
+
+# from app import db
+
+# if not os.path.isfile("vessel.db"):
+#     db.create_all()
+
+
+PATH_TO_CSV = 'vessel_data.csv'
+BASE = "http://127.0.0.1:5000/"
+
+
+    
+def __post_to_db(csv_dict):
+    counter = 0
+    for entry in csv_dict:
+        json_entry = { x.translate({32:None}) : y 
+                 for x, y in entry.items()}
+        # print(f"in {json_entry}")
+        response = requests.post( BASE + "insert", params=json_entry)
+        # print(f" out {response.json()}")
+        
+
+def main():
+    with open(PATH_TO_CSV, mode='r') as csv_file:
+        csv_dict = csv.DictReader(csv_file)
+        __post_to_db(csv_dict)
+
+if __name__ == "__main__":
+    main()
